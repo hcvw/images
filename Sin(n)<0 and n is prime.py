2f378e7f0a596e50ae52008385b29fdff1d15b84
hcvw <31328517+hcvw@users.noreply.github.com>
2017-09-06 14:52:54 -0700

    Delete Sin(n)<0 and n is prime.py

--- Sin(n)<0 and n is prime.py
+++
@@ -1,8 +0,0 @@
-import math
-for i in range(2,100):
-    is_prime = True
-    for j in range(2, int(math.sqrt(i)+1)):
-        if i % j == 0:
-            is_prime = False
-    if is_prime and math.sin(i) < 0:
-        print(i)
